89c51a1b08ca4a83adc2ee325991179a94f7dca0
lfh35 <luca@halbich.net>
2024-07-02 14:00:13 +0200

    added Installing eCAL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,23 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Eclipse eCAL™',
-			customCss: [
-				// Relative path to your custom CSS file
-				'./src/styles/custom.css',
-			],
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Getting Started',
-					autogenerate: { directory: 'gettingStarted' }, //{ label: 'Example Guide', link: '/guides/example/' },
-				},
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: 'Eclipse eCAL™',
+    customCss: [
+    // Relative path to your custom CSS file
+    './src/styles/custom.css'],
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    sidebar: [{
+      label: 'Getting Started',
+      autogenerate: {
+        directory: 'getting_started'
+      } //{ label: 'Example Guide', link: '/guides/example/' },
+    }]
+  }), icon()]
 });
