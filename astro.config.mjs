0cc09fc16e852b4b84d7ee683ac7a1cdffec7fb4
lfh35 <luca@halbich.net>
2024-07-02 13:06:51 +0200

    added Introduction

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,21 +5,18 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Eclipse eCAL™',
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/custom.css',
+			],
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Getting Started',
+					autogenerate: { directory: 'gettingStarted' }, //{ label: 'Example Guide', link: '/guides/example/' },
 				},
 			],
 		}),
